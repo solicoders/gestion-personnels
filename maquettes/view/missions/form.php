78a40a3f95b0865e612bdd438d362b35d0c745d6
imrane sarsri <imransarsi.ii@gmail.com>
2024-03-07 10:19:39 +0100

    save

--- maquettes/view/missions/form.php
+++ maquettes/view/missions/form.php
@@ -1,23 +1,29 @@
 <form action="process_form.php" method="POST">
-    <fieldset class="border col-lg-12 pb-3">
-        <legend>Informations de mission</legend>
+    <fieldset class="border col-lg-12 mb-5 p-3">
+        <legend>Les Informations de mission</legend>
         <div class="row">
             <div class="form-group col-lg-6 col-12">
-                <label for="inputStartDate">Personnel <span class="text-danger">*</span></label>
-                <input name="startDate" type="text" class="form-control" id="inputStartDate"
-                    placeholder="Sélectionnez la date de début" value="madani ali">
+                <label>Personnel</label>
+                <select class="form-control">
+                    <option>madani ali</option>
+                    <option>chami wld lhani</option>
+                    <option>lmchtab hamid</option>
+                </select>
             </div>
-
             <div class="form-group col-lg-6 col-12">
                 <label for="inputEndDate">Numéro de mission <span class="text-danger">*</span></label>
                 <input name="endDate" type="" class="form-control" id="inputEndDate"
                     placeholder="Sélectionnez la nom mission" value="102">
             </div>
-
             <div class="form-group col-lg-6 col-12">
                 <label for="inputEndDate">Nature <span class="text-danger">*</span></label>
                 <input name="endDate" type="" class="form-control" id="inputEndDate"
                     placeholder="Sélectionnez la nature" value="">
+            </div>
+            <div class="form-group col-lg-6 col-12">
+                <label for="inputCatégorie">Catégorie <span class="text-danger">*</span></label>
+                <input name="endDate" type="" class="form-control" id="inputCatégorie"
+                    placeholder="Sélectionnez la Catégorie" value="">
             </div>
             <div class="form-group col-lg-6 col-12">
                 <label for="inputDescription">Type de mission <span class="text-danger">*</span></label>
@@ -29,35 +35,34 @@
                 <input name="nombreDuJour" type="text" class="form-control" id="nombreDuJour"
                     placeholder="Sélectionnez Numéro d'ordre de mission" value="12">
             </div>
-            <div class="col-12 col-lg-6">
+            <div class="form-group col-12 col-lg-6 moyens-de-transport">
                 <label for="date-de-fin">Moyens de transport <span class="text-danger">*</span></label>
                 <select class="form-control">
-                    <option>Voiture de personnel</option>
-                    <option>Transport public</option>
-                    <option>Voiture de mission</option>
+                    <option class="voiture-de-personnel">Voiture de personnel</option>
+                    <option class="transport-public">Transport public</option>
+                    <option class="voiture-de-mission">Voiture de mission</option>
                 </select>
             </div>
-            <div class="col-12 row">
-                <div class="col-lg-6 col-12">
-                    <label for="inputDescription">Marque <span class="text-danger">*</span></label>
-                    <input name="nombreDuJour" type="text" class="form-control" id="nombreDuJour"
-                        placeholder="Sélectionnez Numéro d'ordre de mission" value="Toyota">
-                </div>
-                <div class="col-lg-6 col-12">
-                    <label for="inputDescription">Numéro de plaque <span class="text-danger">*</span></label>
-                    <input name="nombreDuJour" type="text" class="form-control" id="nombreDuJour"
-                        placeholder="Sélectionnez Numéro d'ordre de mission" value="ABC123">
-                </div>
-                <div class="col-lg-6 col-12 pt-3">
-                    <label for="inputDescription">Puissance fiscale <span class="text-danger">*</span></label>
-                    <input name="nombreDuJour" type="text" class="form-control" id="nombreDuJour"
-                        placeholder="Sélectionnez Puissance fiscale" value="4cv">
-                </div>
+            <div class="form-group col-lg-6 col-12">
+                <label for="inputDescription">Marque <span class="text-danger">*</span></label>
+                <input name="nombreDuJour" type="text" class="form-control" id="nombreDuJour"
+                    placeholder="Sélectionnez Numéro d'ordre de mission" value="Toyota">
+            </div>
+            <div class="form-group col-lg-6 col-12">
+                <label for="inputDescription">Numéro de plaque <span class="text-danger">*</span></label>
+                <input name="nombreDuJour" type="text" class="form-control" id="nombreDuJour"
+                    placeholder="Sélectionnez Numéro d'ordre de mission" value="ABC123">
+            </div>
+            <div class="form-group col-lg-6 col-12">
+                <label for="inputDescription">Puissance fiscale <span class="text-danger">*</span></label>
+                <input name="nombreDuJour" type="text" class="form-control" id="nombreDuJour"
+                    placeholder="Sélectionnez Puissance fiscale" value="4cv">
             </div>
         </div>
     </fieldset>
-    <fieldset class="border col-lg-12 pb-2">
-        <legend>Planning de mission</legend>
+    <fieldset class="border col-lg-12 mb-5 p-3">
+        <legend>Planification de mission
+        </legend>
         <div class="row">
             <div class="form-group col-lg-6 col-12">
                 <label for="Lieu">Lieu <span class="text-danger">*</span></label>
